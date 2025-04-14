6cf5bae531742daead56ad9daa8d7e79cfe32744
Jared Narvaez <jarjar0623@gmail.com>
2025-04-14 02:02:08 -0700

    fixed home error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,15 +4,10 @@ export default function Navbar() {
   return (
     <nav className="bg-black p-4 font-[family-name:var(--font-poppins)]">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/home" className="text-white text-xl font-bold">
+        <Link href="/" className="text-white text-xl font-bold">
           My Portfolio
         </Link>
         <ul className="flex space-x-4">
-          <li>
-            <Link href="/home" className="text-white hover:opacity-80">
-              Home
-            </Link>
-          </li>
           <li>
             <Link href="/about" className="text-white hover:opacity-80">
               About
