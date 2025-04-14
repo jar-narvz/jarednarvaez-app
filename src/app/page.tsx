99398c72e1609d6c9976e54b2d96b9c058849d7c
Jared Narvaez <jarjar0623@gmail.com>
2025-04-14 02:04:57 -0700

    deployment error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-8 font-[family-name:var(--font-poppins)]">
